6d0ef486307dc2cc069a628ece838bf6ae511123
Daisuke Satoh <daisuke.satoh@level-five.jp>
2016-05-16 22:35:50 +0900

    add http_post function

--- lib/umakadata/http_helper.rb
+++ lib/umakadata/http_helper.rb
@@ -1,20 +1,59 @@
 require 'net/http'
+require 'umakadata/logging/http_log'
 
 module Umakadata
+
   module HTTPHelper
 
-    def http_get(uri, headers, time_out = 10)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.open_timeout = time_out
-      path = uri.path.empty? ? '/' : uri.path
+    def http_get(uri, args = {})
+      args = {
+        :time_out => 10
+      }.merge(args)
 
+      uri = URI.parse(uri.to_s) unless uri.is_a?(URI)
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.open_timeout = args[:time_out]
+
+      request = Net::HTTP::Get.new(uri.path.empty? ? '/' : uri.path, args[:headers])
+
+      http_log = Umakadata::Logging::HttpLog.new(uri, request)
+
+      execute_request(http, request, http_log, args)
+    end
+
+    def http_post(uri, form_data, args = {})
+      args = {
+        :time_out => 10
+      }.merge(args)
+
+      uri = URI.parse(uri.to_s) unless uri.is_a?(URI)
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.open_timeout = args[:time_out]
+
+      request = Net::HTTP::Post.new(uri.path.empty? ? '/' : uri.path, args[:headers])
+      request.set_form_data(form_data, ';')
+
+      http_log = Umakadata::Logging::HttpLog.new(uri, request)
+
+      execute_request(http, request, http_log, args)
+    end
+
+    def execute_request(http, request, http_log, args = {})
       begin
-        response = http.get(path, headers)
+        response = http_log.response = http.start { |h|
+          h.request(request)
+        }
       rescue => e
-        return e.message
+        http_log.error = e
       end
 
-      return response
+      # append a log object to log container
+      log = args[:logger]
+      log.push http_log unless log.nil?
+
+      response
     end
 
     def http_get_recursive(uri, headers = {}, time_out = 10, limit = 10)
@@ -33,14 +72,15 @@ module Umakadata
       end
 
       case response
-      when Net::HTTPSuccess
-        return response
-      when Net::HTTPRedirection
-        return http_get_recursive(URI(response['location']), headers, time_out, limit - 1)
-      else
-        return response
+        when Net::HTTPSuccess
+          return response
+        when Net::HTTPRedirection
+          return http_get_recursive(URI(response['location']), headers, time_out, limit - 1)
+        else
+          return response
       end
     end
 
   end
+
 end
