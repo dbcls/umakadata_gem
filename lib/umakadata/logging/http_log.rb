9a424bcd83d3eafeb41fcfdfd4cc8cc7e38c0d48
Daisuke Satoh <daisuke.satoh@level-five.jp>
2016-05-16 23:57:47 +0900

    add class for sparql log

--- lib/umakadata/logging/http_log.rb
+++ lib/umakadata/logging/http_log.rb
@@ -5,41 +5,6 @@ module Umakadata
 
   module Logging
 
-    Request = Struct.new(:request) do
-      def to_h
-        case request
-          when Net::HTTP::Get
-            {:method => 'GET', :header => request.each.to_h}
-          when Net::HTTP::Post
-            {:method => 'POST', :header => request.each.to_h, :body => request.body}
-          else
-            {:error => "Unknown type #{request.inspect}"}
-        end
-      end
-    end
-
-    Response = Struct.new(:response) do
-      def to_h
-        case response
-          when Net::HTTPResponse
-            {:code => response.code, :header => response.each.to_h, :body => response.body}
-          else
-            {:error => "Unknown type #{response.inspect}"}
-        end
-      end
-    end
-
-    Error = Struct.new(:exception) do
-      def to_h
-        case exception
-          when StandardError
-            {:message => exception.message}
-          else
-            {:message => exception.to_s}
-        end
-      end
-    end
-
     class HttpLog < Log
 
       attr_writer :response
