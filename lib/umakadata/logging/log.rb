1ba001ea4deecfc0c8f3d572079d94a253b45095
Daisuke Satoh <daisuke.satoh@level-five.jp>
2016-05-18 16:33:51 +0900

    fix bug for log class; fail to convert to json if the request / response body has invalid character code

--- lib/umakadata/logging/log.rb
+++ lib/umakadata/logging/log.rb
@@ -32,13 +32,18 @@ module Umakadata
 
     end
 
+    def force_encode(obj)
+      return obj unless obj.is_a? String
+      obj.force_encoding('ASCII-8BIT').encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => '?')
+    end
+
     Request = Struct.new(:request) do
       def to_h
         case request
           when Net::HTTP::Get
             {:method => 'GET', :header => request.each.to_h}
           when Net::HTTP::Post
-            {:method => 'POST', :header => request.each.to_h, :body => request.body}
+            {:method => 'POST', :header => request.each.to_h, :body => force_encode(request.body)}
           else
             {:error => "Unknown type #{request.inspect}"}
         end
@@ -49,7 +54,7 @@ module Umakadata
       def to_h
         case response
           when Net::HTTPResponse
-            {:code => response.code, :header => response.each.to_h, :body => response.body}
+            {:code => response.code, :header => response.each.to_h, :body => force_encode(response.body)}
           else
             {:error => "Unknown type #{response.inspect}"}
         end
