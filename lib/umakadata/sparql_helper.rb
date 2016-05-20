aab371bc0781f47a4bef96e12a7deabd970a0c80
mako <konno@level-five.jp>
2016-05-20 20:18:44 +0900

    Remove unnecessary variables.

--- lib/umakadata/sparql_helper.rb
+++ lib/umakadata/sparql_helper.rb
@@ -5,18 +5,19 @@ module Umakadata
   module SparqlHelper
 
     def query(uri, query, logger:nil)
-      client = Umakadata::MySparqlClient.new(uri, {'read_timeout': 5 * 60})
-
       sparql_log = Umakadata::Logging::SparqlLog.new(uri, query)
       logger.push sparql_log unless logger.nil?
+
       begin
-        response = @client.query(query)
-        sparql_log.request = @client.request_data
+        client = Umakadata::MySparqlClient.new(uri, {'read_timeout': 5 * 60})
+        response = client.query(query)
+        sparql_log.request = client.request_data
         sparql_log.response = response
       rescue => e
         sparql_log.error = 'Empty triples'
         return nil
       end
+      
       return response
     end
 
