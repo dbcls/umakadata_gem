04a9987e3220a557e67adef1ac7b6c9db5e2557e
Daisuke Satoh <daisuke.satoh@level-five.jp>
2016-05-24 16:13:38 +0900

    change sequence (dafault method in SparqlClient is post)

--- lib/umakadata/criteria/liveness.rb
+++ lib/umakadata/criteria/liveness.rb
@@ -14,7 +14,7 @@ module Umakadata
       def alive?(uri, time_out, logger: nil)
         sparql_query = 'SELECT * WHERE {?s ?p ?o} LIMIT 1'
 
-        [:get, :post].each do |method|
+        [:post, :get].each do |method|
           response = Umakadata::SparqlHelper.query(uri, sparql_query, logger: logger, options: {method: method})
           return true unless response.nil?
         end
