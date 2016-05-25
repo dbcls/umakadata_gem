1a04ac647b1c6b2e047784433509eb35a5850574
mako <konno@level-five.jp>
2016-05-25 21:05:47 +0900

    Change definition to log and create log class for to save criteria

---
+++ lib/umakadata/logging/criteria_log.rb
@@ -0,0 +1,42 @@
+require 'json'
+
+module Umakadata
+
+  module Logging
+
+    class CriteriaLog < Log
+
+      attr_writer :result
+
+      def initialize
+        @list = Array.new
+        @result = nil
+      end
+
+      def push(log)
+        @list.push log
+      end
+
+      def to_h
+        logs = Array.new
+        @list.each { |log|
+          logs.push log.to_h
+        }
+
+        {:result => @result, :logs => logs}
+      end
+
+      def as_json
+        logs = Array.new
+        @list.each { |log|
+          logs.push log.to_h
+        }
+
+        {:result => @result, :logs => logs.to_json}
+      end
+
+    end
+
+  end
+
+end
--- lib/umakadata/logging/log.rb
+++ lib/umakadata/logging/log.rb
@@ -6,11 +6,8 @@ module Umakadata
 
     class Log
 
-      attr_writer :criterion
-
       def initialize
         @list = Array.new
-        @criterion = nil
       end
 
       def push(log)
@@ -22,7 +19,7 @@ module Umakadata
         @list.each { |log|
           result.push log.to_h
         }
-        {'criterion' => @criterion.to_s, 'logs' => result}
+        result
       end
 
       def as_json
@@ -30,7 +27,7 @@ module Umakadata
         @list.each { |log|
           result.push log.to_h
         }
-        {'criterion' => @criterion.to_s, 'logs' => result}.to_json
+        result.to_json
       end
 
     end
