3c6440ad5ce43e333d948fd815b1c18cd56593db
Shunsuke-Komuta <komuta@level-five.jp>
2016-05-26 14:15:33 +0900

    modify valiable name

--- lib/umakadata/criteria/liveness.rb
+++ lib/umakadata/criteria/liveness.rb
@@ -15,14 +15,14 @@ module Umakadata
         sparql_query = 'SELECT * WHERE {?s ?p ?o} LIMIT 1'
 
         [:post, :get].each do |method|
-          method_log = Umakadata::Logging::Log.new
-          logger.push method_log unless logger.nil?
-          response = Umakadata::SparqlHelper.query(uri, sparql_query, logger: method_log, options: {method: method})
+          request_log = Umakadata::Logging::Log.new
+          logger.push request_log unless logger.nil?
+          response = Umakadata::SparqlHelper.query(uri, sparql_query, logger: request_log, options: {method: method})
           unless response.nil?
-            method_log.criterion = "#{method.to_s.capitalize}: 200 HTTP response"
+            request_log.criterion = "#{method.to_s.capitalize}: 200 HTTP response"
             return true
           end
-          method_log.criterion = "#{method.to_s.capitalize}: HTTP response errorr"
+          request_log.criterion = "#{method.to_s.capitalize}: HTTP response errorr"
         end
 
         false
