814bf11e6fc6d185cd391a835643156c302f4a7d
mako <konno@level-five.jp>
2016-05-26 17:10:45 +0900

    Change the definition of http_get_recursive to http_helper

--- lib/umakadata/http_helper.rb
+++ lib/umakadata/http_helper.rb
@@ -56,29 +56,16 @@ module Umakadata
       response
     end
 
-    def http_get_recursive(uri, headers = {}, time_out = 10, limit = 10)
+    def http_get_recursive(uri, args = {}, limit = 10)
       raise RuntimeError, 'HTTP redirect too deep' if limit == 0
 
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.open_timeout = time_out
+      resource = uri.path
+      resource += "?" + uri.query unless uri.query.nil?
+      uri = URI.join(uri.to_s, resource)
+      response = http_get(uri, args)
 
-      begin
-        resource = uri.path
-        resource += "?" + uri.query unless uri.query.nil?
-        response = http.get(resource, headers)
-      rescue => e
-        puts e
-        return e.message
-      end
-
-      case response
-        when Net::HTTPSuccess
-          return response
-        when Net::HTTPRedirection
-          return http_get_recursive(URI(response['location']), headers, time_out, limit - 1)
-        else
-          return response
-      end
+      return http_get_recursive(URI(response['location']), args, limit - 1) if response.is_a? Net::HTTPRedirection
+      return response
     end
 
   end
