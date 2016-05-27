f3dd150e1af2395a509488074ae4c99944b8caab
mako <konno@level-five.jp>
2016-05-27 12:42:41 +0900

    Repair rspec test for VoID

--- spec/umakadata/criteria/void_spec.rb
+++ spec/umakadata/criteria/void_spec.rb
@@ -23,13 +23,13 @@ describe 'Umakadata' do
         it 'should return void object when valid response is retrieved of ttl format' do
           valid_ttl = read_file('good_turtle_01.ttl')
           response = double(Net::HTTPResponse)
-          allow(target).to receive(:http_get_recursive).with(@uri, anything, 10).and_return(response)
+          allow(target).to receive(:http_get_recursive).with(@uri, anything).and_return(response)
           allow(target).to receive(:well_known_uri).and_return(@uri)
-          allow(response).to receive(:is_a?).and_return(true)
+          allow(response).to receive(:is_a?).with(Net::HTTPSuccess).and_return(true)
           allow(response).to receive(:each_key)
           allow(response).to receive(:body).and_return(valid_ttl)
 
-          void = target.void_on_well_known_uri(@uri)
+          void = target.void_on_well_known_uri(@uri, logger: nil)
 
           expect(void.license.include?('http://creativecommons.org/licenses/by/2.1/jp/')).to be true
           expect(void.publisher.include?('http://www.example.org/Publisher')).to be true
@@ -39,7 +39,7 @@ describe 'Umakadata' do
         it 'should return void object when valid response is retrieved of xml format' do
           valid_ttl = read_file('good_xml_01.xml')
           response = double(Net::HTTPResponse)
-          allow(target).to receive(:http_get_recursive).with(@uri, anything, 10).and_return(response)
+          allow(target).to receive(:http_get_recursive).with(@uri, anything).and_return(response)
           allow(target).to receive(:well_known_uri).and_return(@uri)
           allow(response).to receive(:is_a?).and_return(true)
           allow(response).to receive(:body).and_return(valid_ttl)
@@ -53,34 +53,36 @@ describe 'Umakadata' do
 
         it 'should return false description object when invalid response is retrieved' do
           invalid_ttl = read_file('bad_turtle_01.ttl')
-          response = double(Net::HTTPResponse)
-          allow(target).to receive(:http_get_recursive).with(@uri, anything, 10).and_return(response)
+          response = double(Net::HTTPSuccess)
+          logger = Umakadata::Logging::Log.new
+          allow(target).to receive(:http_get_recursive).with(@uri, anything).and_return(response)
           allow(target).to receive(:well_known_uri).and_return(@uri)
           allow(response).to receive(:is_a?).with(Net::HTTPSuccess).and_return(true)
           allow(response).to receive(:body).and_return(invalid_ttl)
 
-          void = target.void_on_well_known_uri(@uri)
+          void = target.void_on_well_known_uri(@uri, logger: logger)
 
           expect(void.text).to be_nil
           expect(void.license).to be_nil
           expect(void.publisher).to be_nil
           expect(void.modified).to be_nil
-          expect(target.get_error).to eq 'Neither turtle nor rdfxml format'
+          expect(logger.to_h[0][:result]).to eq 'VoID can not be retrieved Turtle and RDF/XML format'
         end
 
         it 'should set error message when invalid response is retrieved' do
           response = double(Net::HTTPInternalServerError)
-          allow(target).to receive(:http_get_recursive).with(@uri, anything, 10).and_return(response)
+          allow(target).to receive(:http_get_recursive).with(@uri, anything).and_return(response)
           allow(target).to receive(:well_known_uri).and_return(@uri)
           allow(response).to receive(:is_a?).with(Net::HTTPSuccess).and_return(false)
           allow(response).to receive(:is_a?).with(Net::HTTPResponse).and_return(true)
           allow(response).to receive(:code).and_return('500')
           allow(response).to receive(:message).and_return('Internal Server Error')
+          logger = Umakadata::Logging::Log.new
 
-          void = target.void_on_well_known_uri(@uri)
+          void = target.void_on_well_known_uri(@uri, logger: logger)
 
           expect(void).to be_nil
-          expect(target.get_error).to eq "500 Internal Server Error"
+          expect(logger.to_h[0][:result]).to eq 'The endpoint could not return 200 HTTP response'
         end
 
       end
