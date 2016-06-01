7da503334a7e575283fc403a9e1c77fdbe30034a
Shunsuke-Komuta <komuta@level-five.jp>
2016-06-01 19:05:18 +0900

    change log of service description

--- lib/umakadata/criteria/service_description.rb
+++ lib/umakadata/criteria/service_description.rb
@@ -18,29 +18,31 @@ module Umakadata
       # @option opts [Integer] :time_out Seconds to wait until connection is opened.
       # @return      [Umakadata::ServiceDescription|nil]
       def service_description(uri, time_out, content_type = nil, logger: nil)
+        log = Umakadata::Logging::Log.new
+        logger.push log unless logger.nil?
         headers = {}
         headers['Accept'] = content_type
         headers['Accept'] ||= SERVICE_DESC_CONTEXT_TYPE.join(',')
-        criteria_log = Umakadata::Logging::Log.new
-        logger.push criteria_log unless logger.nil?
-        args = {:headers => headers, :time_out => time_out, :logger => criteria_log}
+        args = {:headers => headers, :time_out => time_out, :logger => log}
 
         response = http_get(uri, args)
 
         if !response.is_a?(Net::HTTPSuccess)
-          criteria_log.result = 'The endpoint could not return 200 HTTP response'
+          log.result = 'The endpoint could not return 200 HTTP response'
+          logger.result = 'The endpoint could not return 200 HTTP response' unless logger.nil?
           return nil
         end
 
+        log.result = 'The endpoint returns 200 HTTP response'
         sd = Umakadata::ServiceDescription.new(response)
 
         case sd.type
         when Umakadata::DataFormat::UNKNOWN
-          criteria_log.result = 'ServiceDescription can not be retrieved in Turtle and RDF/XML format'
+          logger.result = 'ServiceDescription can not be retrieved in Turtle and RDF/XML format' unless logger.nil?
         when Umakadata::DataFormat::TURTLE
-          criteria_log.result = 'ServiceDescription can be retrieved in Turtle format'
+          logger.result = 'ServiceDescription can be retrieved in Turtle format' unless logger.nil?
         when Umakadata::DataFormat::RDFXML
-          criteria_log.result = 'ServiceDescription can be retrieved in RDF/XML format'
+          logger.result = 'ServiceDescription can be retrieved in RDF/XML format' unless logger.nil?
         end
 
         sd
