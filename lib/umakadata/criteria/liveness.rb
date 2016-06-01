e1de4feea4999f1fe9f35f85b9025f6990454297
Shunsuke-Komuta <komuta@level-five.jp>
2016-06-01 20:02:15 +0900

    modify miss implemetation

--- lib/umakadata/criteria/liveness.rb
+++ lib/umakadata/criteria/liveness.rb
@@ -21,12 +21,12 @@ module Umakadata
           response = Umakadata::SparqlHelper.query(uri, sparql_query, logger: request_log, options: {method: method})
           unless response.nil?
             request_log.result = "#{method.to_s.capitalize}: 200 HTTP response"
-            logger.result = "Alive is #{true}" unless logger.nil?
+            logger.result = "Alive is true" unless logger.nil?
             return true
           end
           request_log.result = "#{method.to_s.capitalize}: HTTP response error"
         end
-        logger.result = "Alive is #{false}" unless logger.nil?
+        logger.result = "Alive is false" unless logger.nil?
         false
       end
 
