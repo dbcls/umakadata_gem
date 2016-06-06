a444c6e2f72a42f4ebe407cf1f37493ec25083c7
mako <konno@level-five.jp>
2016-06-06 16:16:52 +0900

    Modify log message in cool_uri

--- lib/umakadata/criteria/cool_uri.rb
+++ lib/umakadata/criteria/cool_uri.rb
@@ -9,34 +9,34 @@ module Umakadata
         cool_uri_log = Umakadata::Logging::CoolUriLog.new(uri)
         logger.push cool_uri_log unless logger.nil?
 
-        rate = 0
+        score = 0
         if uri.host !~ /\d+\.\d+\.\d+\.\d+/
-          rate += 25
-          cool_uri_log.host = 'Matched rule. plus 25 points'
+          score += 25
+          cool_uri_log.host = 'Add 25 points to score'
         else
-          cool_uri_log.host = 'Could not match rule'
+          cool_uri_log.host = 'A Host should not be specified by IP Address'
         end
         if uri.port == 80
-          rate += 25
-          cool_uri_log.port = 'Matched rule. plus 25 points'
+          score += 25
+          cool_uri_log.port = 'Add 25 points to score'
         else
-          cool_uri_log.port = 'Could not match rule'
+          cool_uri_log.port = 'A port of URI should be 80'
         end
         if uri.query.nil?
-          rate += 25
-          cool_uri_log.query = 'Matched rule. plus 25 points'
+          score += 25
+          cool_uri_log.query = 'Add 25 points to score'
         else
-          cool_uri_log.query = 'Could not match rule'
+          cool_uri_log.query = 'A URI should not contain query parameters'
         end
         if uri.to_s.length <= 30
-          rate += 25
-          cool_uri_log.length = 'Matched rule. plus 25 points'
+          score += 25
+          cool_uri_log.length = 'Add 25 points to score'
         else
-          cool_uri_log.length = 'Could not match rule'
+          cool_uri_log.length = 'A length of URI should be less than 30 characters'
         end
 
-        logger.result = "Cool URI Score is #{rate}" unless logger.nil?
-        return rate
+        logger.result = "Cool URI Score is #{score}" unless logger.nil?
+        return score
       end
     end
   end
