99c7b466bf0cf0fff2eb1dfc86cc6154ea9f1614
mako <konno@level-five.jp>
2016-06-07 17:48:34 +0900

    Modify log message in liveness

--- lib/umakadata/criteria/liveness.rb
+++ lib/umakadata/criteria/liveness.rb
@@ -19,18 +19,14 @@ module Umakadata
           request_log = Umakadata::Logging::Log.new
           logger.push request_log unless logger.nil?
           response = Umakadata::SparqlHelper.query(uri, sparql_query, logger: request_log, options: {method: method})
-          if response.is_a? Net::HTTPOK
-            request_log.result = "#{method.to_s.capitalize}: 200 HTTP response"
+          unless response.nil?
+            request_log.result = "200 HTTP response"
             logger.result = "The endpoint is alive" unless logger.nil?
             return true
           end
-          if response.is_a? Net::HTTPResponse
-            request_log.result = "#{method.to_s.capitalize}: #{response.code} HTTP response"
-          else
-            request_log.result = "#{method.to_s.capitalize}: HTTP connection could not be established"
-          end
+          request_log.result = "An error occurred in checking liveness for the endpoint"
         end
-        logger.result = "The endpoint is down" unless logger.nil?
+        logger.result = "The endpoint is not alive" unless logger.nil?
         false
       end
 
