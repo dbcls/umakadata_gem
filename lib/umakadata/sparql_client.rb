351a13ced32112e5d2b8d459e60edb7e8d069952
Daisuke Satoh <daisuke.satoh@level-five.jp>
2016-06-13 17:56:37 +0900

    use hook function to store request / response

--- lib/umakadata/sparql_client.rb
+++ lib/umakadata/sparql_client.rb
@@ -6,30 +6,12 @@ module Umakadata
     attr_reader :http_request
     attr_reader :http_response
 
-    def request(query, headers = {}, &block)
-      headers['Accept'] ||= if (query.respond_to?(:expects_statements?) ?
-                                query.expects_statements? :
-                                (query =~ /CONSTRUCT|DESCRIBE|DELETE|CLEAR/))
-        GRAPH_ALL
-      else
-        RESULT_ALL
-      end
+    def pre_http_hook(request)
+      @http_request = request
+    end
 
-      @http_request = send("make_#{request_method(query)}_request", query, headers)
-
-      @http_request.basic_auth(url.user, url.password) if url.user && !url.user.empty?
-
-      @http_response = @http.request(::URI.parse(url.to_s), @http_request)
-
-      10.times do
-        if @http_response.kind_of? Net::HTTPRedirection
-          @http_response = @http.request(::URI.parse(@http_response['location']), @http_request)
-        else
-          return block_given? ? block.call(@http_response) : @http_response
-        end
-      end
-
-      raise ServerError, "Infinite redirect at #{url}. Redirected more than 10 times."
+    def post_http_hook(response)
+      @http_response = response
     end
 
   end
