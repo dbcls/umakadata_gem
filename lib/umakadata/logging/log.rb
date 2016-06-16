7d1017cff7d6d0f15a4eea4282a6e09f05090c27
TAKATSU Tomonari <takatsu@level-five.jp>
2016-06-16 13:18:18 +0900

    Improve the procedure of encoding

--- lib/umakadata/logging/log.rb
+++ lib/umakadata/logging/log.rb
@@ -38,8 +38,11 @@ module Umakadata
 
     module Util
       def force_encode(obj)
-        return obj unless obj.is_a? String
-        obj.force_encoding('ASCII-8BIT').encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => '?')
+        if obj.is_a? String
+          obj.force_encoding('UTF-8') unless obj.encoding == Encoding::UTF_8
+          obj.encode('UTF-16BE', :invalid => :replace, :undef => :replace, :replace => '?').encode("UTF-8") unless obj.valid_encoding?
+        end
+        return obj
       end
     end
 
