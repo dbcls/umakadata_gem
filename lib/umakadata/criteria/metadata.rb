b168ebd0982b8b1ab0688c2d43a9f4db155d2567
TAKATSU Tomonari <takatsu@level-five.jp>
2016-08-17 14:24:59 +0900

    Fix score at 0

--- lib/umakadata/criteria/metadata.rb
+++ lib/umakadata/criteria/metadata.rb
@@ -133,13 +133,12 @@ module Umakadata
           logger.push graph_log unless logger.nil?
 
           if data[:properties].empty?
-            score =  0
             properties_log = data[:properties_log]
             graph_log.push properties_log
             properties_log.result = '0 ontologies are found'
-            graph_log.result = "Score for #{graph} is #{score}"
+            graph_log.result = "Score for #{graph} is 0"
             logger.result = 'Ontology score is 0'
-            return score
+            return 0
           end
 
           ontologies = self.ontologies(data[:properties])
