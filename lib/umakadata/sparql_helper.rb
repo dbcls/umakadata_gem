88c1d0e412074d9251431fe80dc78b826498e5ef
Daisuke Satoh <daisuke.satoh@level-five.jp>
2018-01-17 17:13:22 +0900

    Revert "retry query on SocketError"
    This reverts commit fab9bb3bfc18d8a2aa5b6a0186616cb2f6f8394e.

--- lib/umakadata/sparql_helper.rb
+++ lib/umakadata/sparql_helper.rb
@@ -5,27 +5,18 @@ module Umakadata
 
   module SparqlHelper
 
-    MAGNIFICATION = lambda { |t| a, b, c = 0, 1, 0; t.times { d = a + b; a = b; b = c = d; }; c }
-
     def self.query(uri, query, logger: nil, options: {})
       sparql_log = Umakadata::Logging::SparqlLog.new(uri, query)
       logger.push sparql_log unless logger.nil?
 
-      retry_count = 0
       begin
         client = Umakadata::SparqlClient.new(uri, {'read_timeout': 5 * 60}.merge(options))
         response = client.query(query)
       rescue RDF::ReaderError
         content_type = client.http_response.content_type
         sparql_log.error = "content-type: #{content_type} is inconsistent with the body of the response"
-      rescue SocketError => e
-        if (retry_count += 1) > 5
-          sparql_log.error = SocketError.new(e.message + "\nRetry count: #{retry_count}")
-        else
-          sparql_log.error = "Retry count: #{retry_count}"
-          sleep 10 * MAGNIFICATION.call(retry_count)
-          retry
-        end
+      rescue SPARQL::Client::ClientError, SPARQL::Client::ServerError => e
+        sparql_log.error = e
       rescue => e
         sparql_log.error = e
       end
