a0e1f14f6e01182c15d9c4e144edcd30f46f4faa
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-07-13 09:28:01 +0900

    initial import

---
+++ lib/umakadata/endpoint/syntax_helper.rb
@@ -0,0 +1,43 @@
+module Umakadata
+  class Endpoint
+    # Helper methods to evaluate endpoint's supports for SPARQL syntax
+    #
+    # @since 1.0.0
+    module SyntaxHelper
+      # Check whether if the endpoint support graph keyword
+      #
+      # @return [true, false] true if the endpoint support graph keyword
+      def graph_keyword_supported?
+        @graph_keyword_supported ||= graph_keyword_support.response.status == 200
+      end
+
+      # Check whether if the endpoint support service keyword
+      #
+      # @return [true, false] true if the endpoint support service keyword
+      def service_keyword_supported?
+        @service_keyword_supported ||= service_keyword_support.response.status == 200
+      end
+
+      # Execute query to check graph keyword support
+      #
+      # @return [Umakadata::Query]
+      def graph_keyword_support
+        @graph_keyword_support ||= sparql
+                                     .construct(%i[s p o])
+                                     .where(%i[s p o])
+                                     .graph(:g)
+                                     .limit(1)
+                                     .execute
+      end
+
+      # Execute query to check service keyword support
+      #
+      # @return [Umakadata::Query]
+      def service_keyword_support
+        @service_keyword_support ||= sparql.query <<~SPARQL
+          CONSTRUCT { ?s ?p ?o . } WHERE { SERVICE <#{url}> { ?s ?p ?o . } } LIMIT 1
+        SPARQL
+      end
+    end
+  end
+end
