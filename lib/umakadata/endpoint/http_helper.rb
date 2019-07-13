af0ffbc07d9e094eeef03e9c31cd19e524bc9099
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-07-13 09:03:33 +0900

    initial import

---
+++ lib/umakadata/endpoint/http_helper.rb
@@ -0,0 +1,22 @@
+module Umakadata
+  class Endpoint
+    # Helper methods to evaluate endpoint's supports for HTTP communication
+    #
+    # @since 1.0.0
+    module HTTPHelper
+      # Check whether if the endpoint returns CORS header
+      #
+      # @return [true, false] true if the endpoint returns CORS header
+      def cors_supported?
+        @cors_supported ||= cors_support.response.headers['Access-Control-Allow-Origin'] == '*'
+      end
+
+      # Execute query to check CORS support
+      #
+      # @return [Umakadata::Query]
+      def cors_support
+        @cors_support ||= sparql.ask(%i[s p o]).execute
+      end
+    end
+  end
+end
