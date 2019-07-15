792c101c3de5792af89b2ff8cecd3f4a5d099e5c
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-07-15 10:58:10 +0900

    fix bug and refactor

--- lib/umakadata/http/response_parser.rb
+++ lib/umakadata/http/response_parser.rb
@@ -5,14 +5,13 @@ module Umakadata
     class ResponseParser
       extend Forwardable
 
-      RESULT_JSON = ::SPARQL::Client::RESULT_JSON
-      RESULT_XML = ::SPARQL::Client::RESULT_XML
-      RESULT_CSV = ::SPARQL::Client::RESULT_CSV
-      RESULT_TSV = ::SPARQL::Client::RESULT_TSV
-      RESULT_BOOL = ::SPARQL::Client::RESULT_BOOL
-      RESULT_BRTR = ::SPARQL::Client::RESULT_BRTR
+      RESULT_BOOL = Regexp.new(Regexp.escape(::SPARQL::Client::RESULT_BOOL)).freeze
+      RESULT_JSON = Regexp.new(Regexp.escape(::SPARQL::Client::RESULT_JSON)).freeze
+      RESULT_XML = Regexp.new(Regexp.escape(::SPARQL::Client::RESULT_XML)).freeze
+      RESULT_CSV = Regexp.new(Regexp.escape(::SPARQL::Client::RESULT_CSV)).freeze
+      RESULT_TSV = Regexp.new(Regexp.escape(::SPARQL::Client::RESULT_TSV)).freeze
 
-      RDF_CONTENT_TYPES = %w[
+      RDF_GRAPH_CONTENT_TYPES = %w[
         text/turtle
         text/n3
         application/n-triples
@@ -29,15 +28,16 @@ module Umakadata
       class << self
         #
         # @param [Umakadata::Query::Response] response
-        # @return [Object]
+        # @return [RDF::Query::Solutions, RDF::Enumerable, true, false, nil]
         def parse(response, &block)
-          new(response.body, content_type: response.headers.content_type, callback: block).parse
+          new(response.body, url: response.url, content_type: response.headers.content_type, callback: block).parse
         end
       end
 
       #
       # @param [String] data response body
       # @param [Hash{Symbol => Object}] options
+      # @option options [#to_s] :url used if response turtle uses <> as IRI
       # @option options [String] :content_type
       # @option options [String] :callback
       def initialize(data, **options)
@@ -60,18 +60,18 @@ module Umakadata
         options[:logger] = NULL_LOGGER
 
         case options[:content_type]
-        when Regexp.escape(RESULT_BOOL)
-          'true'
-        when Regexp.escape(RESULT_JSON)
+        when RESULT_BOOL
+          true
+        when RESULT_JSON
           parse_json_bindings(@data)
-        when Regexp.escape(RESULT_XML)
+        when RESULT_XML
           parse_xml_bindings(@data)
-        when Regexp.escape(RESULT_CSV)
+        when RESULT_CSV
           parse_csv_bindings(@data)
-        when Regexp.escape(RESULT_TSV)
+        when RESULT_TSV
           parse_tsv_bindings(@data)
         else
-          parse_rdf_serialization(options) || parse_any_rdf_serialization(options)
+          parse_rdf_serialization(options) || parse_any_rdf_serialization(options) || parse_any_rdf_bindings(options)
         end
       end
 
@@ -80,12 +80,23 @@ module Umakadata
       # @option options [String] :content_type
       # @return [RDF::Enumerable, nil]
       def parse_rdf_serialization(**options)
+        options = options.dup
+
+        # suppress parser error messages
+        options[:logger] = NULL_LOGGER
+
         reader = RDF::Reader.for(options)
 
         begin
           if reader.new(@data).valid?
             callback&.call(reader, nil)
-            reader.new(@data)
+            reader.new(@data).to_a
+          elsif RDF::Turtle::Format.content_type.include?(options[:content_type])
+            data = @data.gsub('<>', "<#{@options[:url]}>")
+            if reader.new(data).valid?
+              callback&.call(reader, nil)
+              reader.new(data).to_a
+            end
           end
         rescue StandardError
           nil
@@ -103,7 +114,7 @@ module Umakadata
         # suppress parser error messages
         options[:logger] = NULL_LOGGER
 
-        RDF_CONTENT_TYPES.each do |type|
+        RDF_GRAPH_CONTENT_TYPES.each do |type|
           options[:content_type] = type
 
           next unless (reader = parse_rdf_serialization(options))
@@ -111,8 +122,40 @@ module Umakadata
           msg = content_type != type ? "Inconsistent content type: response = #{content_type}, parser = #{type}" : nil
           callback&.call(reader, msg)
 
-          break reader
+          return reader
         end
+
+        nil
+      end
+
+      #
+      # @param [Hash{Symbol => Object}] options
+      # @option options [String] :content_type
+      # @return [RDF::Query::Solutions, nil]
+      def parse_any_rdf_bindings(**options)
+        options = options.dup
+        content_type = options[:content_type]
+
+        # suppress parser error messages
+        options[:logger] = NULL_LOGGER
+
+        %i[parse_json_bindings parse_xml_bindings parse_csv_bindings parse_tsv_bindings].each do |method|
+          solutions = begin
+                        __send__(method, @data)
+                      rescue StandardError
+                        nil
+                      end
+
+          next unless solutions
+          next if solutions.count.zero?
+
+          msg = "Inconsistent content type: response = #{content_type}, parser = #{method.to_s.split('_')[1]}"
+          callback&.call(solutions, msg)
+
+          return solutions
+        end
+
+        nil
       end
 
       def callback
