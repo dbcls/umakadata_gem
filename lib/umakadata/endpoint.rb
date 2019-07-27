13b08503554acc804a7402c92f15ea1e385c9713
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-07-27 16:11:59 +0900

    initial import

---
+++ lib/umakadata/endpoint.rb
@@ -0,0 +1,39 @@
+require 'forwardable'
+require 'umakadata/endpoint/http_helper'
+require 'umakadata/endpoint/service_description_helper'
+require 'umakadata/endpoint/syntax_helper'
+require 'umakadata/endpoint/void_helper'
+
+module Umakadata
+  # A SPARQL endpoint for Umakadata
+  #
+  # @!attribute [r] url
+  #   @return [String] the URL of the SPARQL endpoint
+  class Endpoint
+    extend Forwardable
+
+    include HTTPHelper
+    include ServiceDescriptionHelper
+    include SyntaxHelper
+    include VoIDHelper
+
+    attr_reader :url
+
+    # @param [String] url an URL of the SPARQL endpoint
+    def initialize(url, **options)
+      @url = url
+      @options = options
+      @criteria = {}
+    end
+
+    # @return [Umakadata::SPARQL::Client] SPARQL Client
+    def sparql
+      @sparql ||= Umakadata::SPARQL::Client.new(url, **@options)
+    end
+
+    # @return [Umakadata::HTTP::Client] HTTP Client
+    def http
+      @http ||= Umakadata::HTTP::Client.new(url, **@options)
+    end
+  end
+end
