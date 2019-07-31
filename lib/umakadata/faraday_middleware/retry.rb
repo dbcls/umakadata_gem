821e6e8f0c0e6fcf39f0edc19313b7f3c1c26e57
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-07-31 13:53:32 +0900

    change default of retry times to 3

--- lib/umakadata/faraday_middleware/retry.rb
+++ lib/umakadata/faraday_middleware/retry.rb
@@ -4,7 +4,7 @@ module Umakadata
   module FaradayMiddleware
     class Retry < Faraday::Request::Retry
       DEFAULT_OPTIONS = {
-        max: 5,
+        max: 3,
         interval: 10,
         backoff_factor: 2,
         methods: %i[get post]
