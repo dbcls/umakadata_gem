271c4ca0b040c04aac54d752418e41ccadf78d73
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-08-21 01:41:41 +0900

    initial import

---
+++ lib/umakadata/criteria/helpers/usefulness_helper.rb
@@ -0,0 +1,133 @@
+require 'umakadata/util/cacheable'
+require 'umakadata/util/string'
+
+module Umakadata
+  class Criteria
+    module Helpers
+      module UsefulnessHelper
+        include Cacheable
+        include StringExt
+
+        # @return [Umakadata::Activity]
+        def graphs(**options)
+          cache(:graphs, options) do
+            if endpoint.graph_keyword_supported?
+              endpoint.sparql.select(:g).distinct.graph(:g).where(%i[s p o]).execute.tap do |act|
+                act.type = Activity::Type::GRAPHS
+                act.comment = if act.result.present?
+                                "#{pluralize(act.result.count, 'graph')} found."
+                              else
+                                'No graphs found.'
+                              end
+              end
+            else
+              endpoint.graph_keyword_support
+            end
+          end
+        end
+
+        # @param [Hash{Symbol => Object}] options
+        # @option options [String] :graph
+        # @return [Umakadata::Activity]
+        def classes(**options)
+          cache(:classes, options) do
+            buffer = ['PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>']
+            buffer << 'SELECT DISTINCT ?c WHERE {'
+            buffer << "GRAPH <#{options[:graph]}> {" if options[:graph]
+            buffer << '{ ?c a rdfs:Class . }'
+            buffer << 'UNION { [] a ?c . }'
+            buffer << 'UNION { [] rdfs:domain ?c . }'
+            buffer << 'UNION { [] rdfs:range ?c . }'
+            buffer << 'UNION { ?c rdfs:subClassOf [] . }'
+            buffer << 'UNION { [] rdfs:subClassOf ?c . }'
+            buffer << '}' if options[:graph]
+            buffer << '} LIMIT 100'
+
+            endpoint.sparql.query(buffer.join(' ')).tap do |act|
+              act.type = Activity::Type::CLASSES
+              act.comment = if act.result.present?
+                              "#{pluralize(act.result.count, 'class')} found."
+                            else
+                              'No classes found.'
+                            end
+            end
+          end
+        end
+
+        # @param [Hash{Symbol => Object}] options
+        # @option options [String] :graph
+        # @return [Umakadata::Activity]
+        def classes_having_instance(**options)
+          cache(:classes_having_instance, options) do
+            endpoint
+              .sparql
+              .select(:c)
+              .distinct
+              .tap { |x| x.graph(options[:graph]) if options[:graph] }
+              .where([::RDF::BlankNode.new, ::RDF::RDFV.type, :c])
+              .execute
+              .tap do |act|
+              act.type = Activity::Type::CLASSES_HAVING_INSTANCE
+              act.comment = if act.result.present?
+                              "#{pluralize(act.result.count, 'instance')} found."
+                            else
+                              'No instances found.'
+                            end
+            end
+          end
+        end
+
+        # @param [Array<String, #to_s>] classes
+        # @param [Hash{Symbol => Object}] options
+        # @option options [String] :graph
+        # @return [Umakadata::Activity]
+        def labels_of_classes(classes, **options)
+          if Array(classes).empty?
+            return Activity.new do |act|
+              act.result = []
+              act.type = Activity::Type::LABELS_OF_CLASSES
+              act.comment = 'Classes empty.'
+            end
+          end
+
+          endpoint
+            .sparql
+            .select(:c, :label)
+            .distinct
+            .tap { |x| x.graph(options[:graph]) if options[:graph] }
+            .where([:c, ::RDF::Vocab::RDFS.label, :label])
+            .values(:c, *Array(classes))
+            .execute
+            .tap do |act|
+            act.type = Activity::Type::LABELS_OF_CLASSES
+            act.comment = if act.result.present?
+                            "#{pluralize(act.result.count, 'instance')} found."
+                          else
+                            'No instances found.'
+                          end
+          end
+        end
+
+        # @return [Umakadata::Activity]
+        def number_of_statements(**options)
+          cache(:number_of_statements, options) do
+            endpoint
+              .sparql
+              .select(count: { '*' => :count })
+              .where(%i[s p o])
+              .tap { |x| x.graph(:g) if options[:graph] }
+              .execute
+              .tap do |act|
+              act.type = Activity::Type::NUMBER_OF_STATEMENTS
+              act.comment = if act.result.is_a?(Array) && (c = act.result.map { |r| r.bindings[:count] }.first)
+                              "The number of statements is #{c}."
+                            else
+                              'Failed to count the number of statements.'
+                            end
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/umakadata/criteria/usefulness.rb
@@ -0,0 +1,92 @@
+require 'umakadata/criteria/helpers/usefulness_helper'
+require 'umakadata/util/cacheable'
+require 'umakadata/util/string'
+
+module Umakadata
+  class Criteria
+    class Usefulness < Criteria
+      include Cacheable
+      include Helpers::UsefulnessHelper
+
+      MEASUREMENT_NAMES = {
+        ontology: 'usefulness.ontology',
+        metadata: 'usefulness.metadata',
+        links_to_other_datasets: 'usefulness.links_to_other_datasets',
+        data_entry: 'usefulness.data_entry',
+        content_negotiation_supported?: 'usefulness.content_negotiation_supported',
+        turtle_format_supported?: 'usefulness.turtle_format_supported'
+      }.freeze
+
+      #
+      # @yield [measurement]
+      # @yieldparam [Umakadata::Measurement]
+      #
+      # @return [true, false] true if the endpoint is alive
+      def metadata
+        activities = []
+        activities << endpoint.graph_keyword_support
+
+        if endpoint.graph_keyword_supported?
+          activities << (grs = graphs)
+
+          grs.result.map { |r| r.bindings[:g] }.each do |g|
+            next if graph_in_exclude_list(grs, g)
+
+            activities << classes_having_instance(graph: g)
+            activities << labels_of_classes(classes(graph: g).result.map { |r| r.bindings[:c] }, graph: g)
+          end
+        end
+
+        activities << classes_having_instance
+        activities << labels_of_classes(classes.result.map { |r| r.bindings[:c] })
+
+        n = (target = activities.filter { |a| a.result.is_a? Array }).size
+        avg = target.inject(0) { |memo, t| memo + (t.result.size.positive? ? 0 : 100) } / n.to_f
+        comment = if n.positive?
+                    "Metadata score is #{avg}"
+                  else
+                    'There are no effective graphs.'
+                  end
+
+        measurement = Measurement.new(MEASUREMENT_NAMES[__method__], comment, activities)
+
+        yield measurement if block_given?
+
+        avg
+      end
+
+      def ontology
+      end
+
+      def links_to_other_datasets
+      end
+
+      def data_entry
+      end
+
+      def content_negotiation_supported?
+      end
+
+      def turtle_format_supported?
+      end
+
+      private
+
+      def graph_in_exclude_list(activity, graph)
+        uri = RDF::URI(graph)
+
+        unless uri.scheme.match?(/https?/)
+          activity.comment += "\n#{graph} is omitted because the URI does not start with http:// or https://."
+          return true
+        end
+
+        if uri.host == 'www.w3.org' || uri.host == 'www.openlinksw.com' || uri.path == '/DAV'
+          activity.comment += "\n#{graph} is omitted because the URI seems to be prepared by the triple store as default."
+          return true
+        end
+
+        false
+      end
+    end
+  end
+end
