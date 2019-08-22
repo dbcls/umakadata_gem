17c5ccd31c9a5956e372bcf288cc870a5cda9b14
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-08-22 11:29:06 +0900

    refactor

--- lib/umakadata/logger.rb
+++ lib/umakadata/logger.rb
@@ -1,1 +1,19 @@
-require 'umakadata/logger/formatter'
+require 'logger'
+
+module Umakadata
+  module Logger
+    class Formatter < ::Logger::Formatter
+      FORMAT = "%s, [%s#%d] %5s -- %s: %s\n".freeze
+
+      def initialize
+        super
+
+        @datetime_format ||= '%Y-%m-%dT%H:%M:%S.%3N '
+      end
+
+      def call(severity, time, progname, msg)
+        format(FORMAT, severity[0..0], format_datetime(time), $PID, severity, progname, msg2str(msg))
+      end
+    end
+  end
+end
