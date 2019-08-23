ba2d98e19b53b813919bbff71f02465cf215395d
Daisuke Satoh <daisuke.satoh@lifematics.co.jp>
2019-08-23 12:18:22 +0900

    refactor

--- lib/umakadata/models.rb
+++ lib/umakadata/models.rb
@@ -1,2 +1,3 @@
 require 'umakadata/models/activity'
 require 'umakadata/models/measurement'
+require 'umakadata/models/linked_open_vocabulary'
--- lib/umakadata/models/linked_open_vocabulary.rb
+++ lib/umakadata/models/linked_open_vocabulary.rb
@@ -14,20 +14,37 @@ module Umakadata
       #
       # @return [Array<Object>, nil]
       def all
-        path = File.join(Umakadata::Crawler.config.app_home, LOV_CACHE_FILE_NAME)
+        update
+        lov_from_cache.map { |x| x['nsp'] }
+      end
 
-        lov_from_cache(path) || lov_from_remote do |lov|
-          File.open(path, 'w') { |f| f.write JSON.dump(lov) }
+      def update(force: false)
+        if File.exist?(cache_path)
+          return unless force || expired?
         end
+
+        logger.info('Updating cache for Linked Open Vocabularies')
+
+        lov_from_remote do |lov|
+          File.open(cache_path, 'w') { |f| f.write JSON.dump(lov) }
+          logger.info('Successfully updated cache for Linked Open Vocabularies')
+        end
+      end
+
+      def expired?
+        return unless File.exist?(cache_path)
+
+        Time.now - File::Stat.new(cache_path).mtime > 60 * 60 * 24 # 1 day
       end
 
       private
 
-      def lov_from_cache(path)
-        return unless File.exist?(path)
-        return unless ((Time.now - File::Stat.new(path).mtime) / (60 * 60 * 24)).positive?
+      def cache_path
+        File.join(Umakadata::Crawler.config.app_home, LOV_CACHE_FILE_NAME)
+      end
 
-        JSON.parse(File.read(path))
+      def lov_from_cache
+        JSON.parse(File.read(cache_path)) if File.exist?(cache_path)
       end
 
       def lov_from_remote
@@ -40,6 +57,10 @@ module Umakadata
         Array(activity.result).tap do |result|
           yield result if block_given?
         end
+      end
+
+      def logger
+        @logger ||= ::Logger.new((options = Umakadata::Crawler.config.logger.options).delete(:logdev), options)
       end
     end
   end
